fe90a09fad711038e0e50482bccd909be8e4a389
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2026-06-17 19:48:34 +0000

    started building the calendar widget, wip

--- app/src/main/java/com/doukous/hijrawidgetdemo/widgets/GeneralCalendar/Widget.kt
+++ app/src/main/java/com/doukous/hijrawidgetdemo/widgets/GeneralCalendar/Widget.kt
@@ -7,27 +7,92 @@ import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.color.ColorProvider
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.padding
+import androidx.glance.layout.size
 import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
+import com.doukous.hijrawidgetdemo.R
+import com.doukous.hijrawidgetdemo.widgets.data.AppDate
+
 
 class Widget: GlanceAppWidget() {
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        val appDate = AppDate()
+
         provideContent {
-            WidgetContent()
+            WidgetContent(appDate)
         }
     }
 
     @Composable
-    fun WidgetContent() {
-        Text(
-            "Here is the calendar widget",
+    fun WidgetContent(appDate: AppDate) {
+        Column(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = GlanceModifier
                 .background(Color.White)
-                .padding(4.dp)
                 .fillMaxSize()
-        )
+        ) {
+            Row(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                appDate.daysInitials.map { letter ->
+                    Text(
+                        letter,
+                        style = TextStyle(
+                            textAlign = TextAlign.Center,
+                            color = ColorProvider(day=Color.Gray, night=Color.Gray)
+                        ),
+                        modifier = GlanceModifier
+                            .size(24.dp)
+                    )
+                }
+            }
+
+            (0..4).map { week ->
+                Column(modifier = GlanceModifier) {
+                    Row(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        (
+                                week * 7 + 1
+                                        ..
+                                        if (week + 1 * 7 < appDate.todayDateHijrah.lengthOfMonth())
+                                                (week + 1) * 7 else appDate.todayDateHijrah.lengthOfMonth()
+                        )
+                            .map { day ->
+                            if (day == appDate.hijraDay)
+                                Text(
+                                    day.toString(),
+                                    style = TextStyle(
+                                        textAlign = TextAlign.Center,
+                                        color = ColorProvider(day=Color.White, night = Color.White)
+                                    ),
+                                    modifier = GlanceModifier
+                                        .size(24.dp)
+                                        .background(R.color.teal_700)
+                                        .cornerRadius(8.dp)
+                                )
+                            else
+                                Text(
+                                    day.toString(),
+                                    style = TextStyle(textAlign = TextAlign.Center),
+                                    modifier = GlanceModifier
+                                        .size(24.dp)
+                                )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
